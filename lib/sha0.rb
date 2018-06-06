54ec1e94b2949a33841933ca9b8452804b6382c6
tyage <namatyage@gmail.com>
2018-06-06 16:58:09 +0900

    Pad string

--- lib/sha0.rb
+++ lib/sha0.rb
@@ -3,17 +3,21 @@ require "openssl"
 
 K = [0x5a827999, 0x6ed9eba1, 0x8f1bbcdc, 0xca62c1d6];
 
+def rotate_left(value, shift)
+  return ( ((value << shift) | (value >> (32 - shift))) & 0xffffffff)
+end
+
 module SHA0
   # Your code goes here...
   class Digest
     def initialize()
       @data = ''
       @hash = [
-        0x67452301 | 0,
-        0xefcdab89 | 0,
-        0x98badcfe | 0,
-        0x10325476 | 0,
-        0xc3d2e1f0 | 0
+        0x67452301,
+        0xefcdab89,
+        0x98badcfe,
+        0x10325476,
+        0xc3d2e1f0
       ]
       @rounds = 80
       @tmp = []
@@ -21,52 +25,72 @@ module SHA0
 
     def update(data)
       @data += data
+      self
+    end
+
+    def padding(message)
+      bit_string = message.unpack('B*')[0]
+      message_length = bit_string.length
+      bit_string += '1'
+      while ((448 % 512) != (bit_string.length % 512)) do
+        bit_string += '0'
+      end
+      bit_string += (('0' * (64 - message_length.to_s(2).length)) + message_length.to_s(2))
+      [bit_string].pack('B*').unpack('N*')
     end
 
     def hexdigest()
-      hash = OpenSSL::Digest.new('sha1')
-      hash.update(@data)
-      hash.hexdigest
+      @hash = [
+        0x67452301,
+        0xefcdab89,
+        0x98badcfe,
+        0x10325476,
+        0xc3d2e1f0
+      ]
+
+      pad_string = padding(@data)
+      chunks = (pad_string.length / 16).times.collect { |i| pad_string[i * 16, 16] }
+      chunks.each do |chunk|
+        process_block(chunk)
+      end
+      @hash.each_with_object('') do |partial, hash|
+        hash << '0' * (8 - partial.to_s(16).length) + partial.to_s(16)
+      end
     end
 
     def process_block(block)
-      hash0 = @hash[0] | 0;
-      hash1 = @hash[1] | 0;
-      hash2 = @hash[2] | 0;
-      hash3 = @hash[3] | 0;
-      hash4 = @hash[4] | 0;
+      working_vars = Array.new(@hash)
 
       @rounds.times.each do |i|
         if i < 16
-          @tmp[i] = block[i] | 0
+          @tmp[i] = block[i]
         else
-          @tmp[i] = (@tmp[i - 3] ^ @tmp[i - 8] ^ @tmp[i - 14] ^ @tmp[i - 16]) | 0
+          @tmp[i] = (@tmp[i - 3] ^ @tmp[i - 8] ^ @tmp[i - 14] ^ @tmp[i - 16])
         end
 
-        t = (rotate_left(hash0, 5) + hash4 + @tmp[i] + K[i / 20]) | 0
         if i < 20
-          t = (t + ((hash1 & hash2) | (~hash1 & hash3))) | 0
+          f = (working_vars[1] & working_vars[2]) | (~working_vars[1] & working_vars[3])
         elsif i < 40
-          t = (t + (hash1 ^ hash2 ^ hash3)) | 0
+          f = working_vars[1] ^ working_vars[2] ^ working_vars[3]
         elsif i < 60
-          t = (t + ((hash1 & hash2) | (hash1 & hash3) | (hash2 & hash3))) | 0
+          f = (working_vars[1] & working_vars[2]) | (working_vars[1] & working_vars[3]) | (working_vars[2] & working_vars[3])
         else
-          t = (t + (hash1 ^ hash2 ^ hash3)) | 0
+          f = working_vars[1] ^ working_vars[2] ^ working_vars[3]
         end
-        hash4 = hash3
-        hash3 = hash2
-        hash2 = rotate_left(hash1, 30) | 0
-        hash1 = hash0
-        hash0 = t
+        t = (rotate_left(working_vars[0], 5) + working_vars[4] + @tmp[i] + K[i / 20] + f) & 0xffffffff
+
+        working_vars = [
+          t,
+          working_vars[0],
+          rotate_left(working_vars[1], 30),
+          working_vars[2],
+          working_vars[3]
+        ]
       end
 
-      @hash = [
-        (@hash[0] + hash0) | 0,
-        (@hash[1] + hash1) | 0,
-        (@hash[2] + hash2) | 0,
-        (@hash[3] + hash3) | 0,
-        (@hash[4] + hash4) | 0
-      ]
+      working_vars.each_with_index { |a, i|
+        @hash[i] = (@hash[i] + a) & 0xffffffff
+      }
     end
   end
 end
